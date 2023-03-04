b5c1d21f6876ac81695f1c53de1d95e51a645592
talqq16 <talqq16@gmail.com>
2023-03-04 13:06:31 +0200

    Finished Cleaning

--- Script.R
+++ Script.R
@@ -8,9 +8,11 @@ data_filtered_2 = data_filtered[data_filtered$X55.59 != -1,]
 ##Cleaning empty columns
 data_filtered_2 = data_filtered_2[,-10]
 ##Grouping the data by country and summing the population column and the numbers of people exposed to noise columns
-grouped_data = data_filtered_2 %>% group_by(Country) %>% summarise(Population_Sum = sum(Population), "55-59" = sum(X55.59), "60-64" = sum(X60.64), "65-69" = sum(X65.69), "70-74" = sum(X70.74), "75+" = sum(X.75))
+grouped_data = data_filtered_2 %>% group_by(Country) %>% summarise(Population_Sum = sum(Population), Land = sum(Area) , "55-59" = sum(X55.59), "60-64" = sum(X60.64), "65-69" = sum(X65.69), "70-74" = sum(X70.74), "75+" = sum(X.75))
 ## Removing NA's
 no_NAs = na.omit(grouped_data)
 ##Saving the new data as a CSV
 write.csv(no_NAs, "cleaned_data.csv")
-
+cleaned_data = read.csv("cleaned_data.csv")
+##Changing Columns names 
+names(cleaned_data) = c("Country_Number" , "Country" , "Population", "Area" , "55-59", "60-64", "65-69", "70-74", "75+" )
